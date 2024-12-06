83f0fe1cb632de7a2e61c1b29f40287d327cb1f8
Jose Juan Zapote Baltazar <96960077+PMSXAA@users.noreply.github.com>
2024-12-06 16:18:52 -0600

    sdf
    dssd

--- scr/main/java/pokemon/soap/package-info.java
+++ scr/main/java/pokemon/soap/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.2 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.12.06 a las 10:12:35 AM CST 
+// Generado el: 2024.12.06 a las 04:14:21 PM CST 
 //
 
 @jakarta.xml.bind.annotation.XmlSchema(namespace = "http://soap.pokemon/", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
